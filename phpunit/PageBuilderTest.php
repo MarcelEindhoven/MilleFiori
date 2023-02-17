ab4224110e1a6dcc9d6dceb815377246f51e9bd1
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-02-17 21:18:26 +0100

    PageBuilderTest

---
+++ phpunit/PageBuilderTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace NieuwenhovenGames\StarTurner;
+/**
+ *------
+ * StarTurner implementation unit tests : © Marcel van Nieuwenhoven marcel.eindhoven@hotmail.com
+ *
+ */
+
+include_once(__DIR__.'/../vendor/autoload.php');
+use PHPUnit\Framework\TestCase;
+
+class PageBuilderTest extends TestCase{
+}
+?>
