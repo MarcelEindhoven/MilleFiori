0cb9850e117cd2f35f11c8e78b71d20377611fce
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-02-17 21:28:23 +0100

    class PageBuilder

---
+++ export/modules/PageBuilder.php
@@ -0,0 +1,19 @@
+<?php
+namespace NieuwenhovenGames\MilleFiori;
+/**
+ *------
+ * MilleFiori implementation : © Marcel van Nieuwenhoven marcel.eindhoven@hotmail.com
+ * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
+ * See http://en.doc.boardgamearena.com/Studio for more information.
+ *
+ */
+
+class PageBuilder {
+    public function setPage($page)
+    {
+        $this->page = $page;
+        return $this;
+    }
+}
+
+?>
--- phpunit/PageBuilderTest.php
+++ phpunit/PageBuilderTest.php
@@ -9,6 +9,27 @@ namespace NieuwenhovenGames\MilleFiori;
 include_once(__DIR__.'/../vendor/autoload.php');
 use PHPUnit\Framework\TestCase;
 
+include_once(__DIR__.'/../export/modules/PageBuilder.php');
+
+class PageInterface {
+    public function begin_block() {}
+    public function reset_subblocks() {}
+    public function insert_block() {}
+}
+
 class PageBuilderTest extends TestCase{
+    protected PageBuilder $builder;
+
+    protected function setUp(): void {
+        $this->mock = $this->getMockBuilder(PageInterface::class)->getMock();
+        $this->builder = new PageBuilder();
+        $this->builder->setPage($this->mock);
+    }
+
+    public function testEmptyHeaderOnly() {
+        // Arrange
+        // Act
+        // Assert
+    }
 }
 ?>
