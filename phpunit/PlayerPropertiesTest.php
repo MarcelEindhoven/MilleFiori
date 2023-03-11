9a65c8676cfed9c228db1b4ec0e9ae3f757e3974
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-03-11 07:34:02 +0100

    getRobotsCreateQuery

--- phpunit/PlayerPropertiesTest.php
+++ phpunit/PlayerPropertiesTest.php
@@ -21,24 +21,41 @@ class PlayerPropertiesTest extends TestCase{
     }
     public function arrange($number_players, $number_colors) {
         $this->players = [];
+        for ($i=0; $i<$number_players; $i++) {
+            $this->players[$i] = ['player_canal' => 0, 'player_name' => 'player_' . $i, 'player_avatar' => ''];
+        }
+
+        $query_create_players = $this->getPlayersCreateQuery($number_players);
+        $query_create_robots = $this->getRobotsCreateQuery($number_players);
+
+        if ($query_create_robots) {
+            $this->mock->expects($this->exactly(2))->method('query')->withConsecutive([$this->equalTo($query_create_players)], [$this->equalTo($query_create_robots)]);
+        } else {
+            $this->mock->expects($this->exactly(1))->method('query')->with($this->equalTo($query_create_players));
+        }
+    }
+
+    private function getRobotsCreateQuery($number_players) {
+        if ($number_players >= 4) {
+            return null;
+        }
+        $query_robot = PlayerProperties::CREATE_ROBOTS;
+        for ($index = 0; $index < 4 - $number_players; $index++) {
+            $query_robot .= $this->optionalSeparator($index);
+            $query_robot .= "('$index','" . PlayerPropertiesTest::COLORS[$number_players + $index] . "','robot_$index','0')";
+        }
+        return $query_robot;
+    }
+
+    private function getPlayersCreateQuery($number_players) {
         $query = PlayerProperties::CREATE_PLAYERS;
         for ($i=0; $i<$number_players; $i++) {
             $query .= $this->optionalSeparator($i);
             $query .= "('$i','" . PlayerPropertiesTest::COLORS[$i] . "','0','player_$i','','0')";
-
-            $this->players[$i] = ['player_canal' => 0, 'player_name' => 'player_' . $i, 'player_avatar' => ''];
         }
-        if ($number_players < 4) {
-            $query_robot = PlayerProperties::CREATE_ROBOTS;
-            for ($index = 0; $index < 4 - $number_players; $index++) {
-                $query_robot .= $this->optionalSeparator($index);
-                $query_robot .= "('$index','" . PlayerPropertiesTest::COLORS[$number_players + $index] . "','robot_$index','0')";
-            }
-            $this->mock->expects($this->exactly(2))->method('query')->withConsecutive([$this->equalTo($query)], [$this->equalTo($query_robot)]);
-        } else {
-            $this->mock->expects($this->exactly(1))->method('query')->with($this->equalTo($query));
-        }
+        return $query;
     }
+
     private function optionalSeparator(int $index) : string {
         if ($index > 0) {
             return ",";
