5e7787bea5f011d882cc63a46abf743d3f338aae
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-04-10 09:04:55 +0200

    testCreate_MultipleFieldMultipleValues_Query

--- export/modules/BGA/Storage.php
+++ export/modules/BGA/Storage.php
@@ -8,6 +8,42 @@ namespace NieuwenhovenGames\BGA;
  *
  */
 
+include_once(__DIR__.'/DatabaseInterface.php');
+
 class Storage {
+
+    static public function create($sql_database) : Storage {
+        $object = new Storage();
+        return $object->setDatabase($sql_database);
+    }
+
+    public function setDatabase($sql_database) : Storage {
+        $this->sql_database = $sql_database;
+        return $this;
+    }
+
+    protected function getQueryStringField(array $values_field) : string {
+        $values_query_strings_field = [];
+        foreach ($values_field as $value) {
+            $values_query_strings_field[] = "'$value'";
+        }
+
+        return "(" . implode(',', $values_query_strings_field) .")";
+    }
+
+    protected function getQueryStringAllValues(array $values) : string {
+        $values_query_strings = [];
+        foreach ($values as $values_field) {
+            $values_query_strings[] = $this->getQueryStringField($values_field);
+        }
+        return '' . implode(',', $values_query_strings);
+    }
+
+    public function createBucket(string $bucket_name, array $bucket_fields, array $initial_values) {
+        $field_ids_query_string = '' . implode(',', $bucket_fields);
+        $initial_values_query_string = $this->getQueryStringAllValues($initial_values);
+        $sql = "INSERT INTO $bucket_name ($field_ids_query_string) VALUES $initial_values_query_string";
+        $this->sql_database->query($sql);
+    }
 }
 ?>
--- phpunit/StorageTest.php
+++ phpunit/StorageTest.php
@@ -10,12 +10,54 @@ include_once(__DIR__.'/../vendor/autoload.php');
 use PHPUnit\Framework\TestCase;
 
 include_once(__DIR__.'/../export/modules/BGA/Storage.php');
+include_once(__DIR__.'/../export/modules/BGA/DatabaseInterface.php');
 
 class StorageTest extends TestCase{
-    protected Storage $sut;
+    protected \NieuwenhovenGames\BGA\Storage $sut;
 
     protected function setUp(): void {
-        $this->sut = new Storage();
+        $this->mock_database = $this->createMock(\NieuwenhovenGames\BGA\DatabaseInterface::class);
+        $this->sut = \NieuwenhovenGames\BGA\Storage::create($this->mock_database);
+    }
+
+    protected function arrangeCreate($expected_query) {
+        $this->mock_database->expects($this->exactly(1))
+        ->method('query')
+        ->with($this->equalTo($expected_query));
+    }
+
+    public function testCreate_NoField_SimpleQuery() {
+        // Arrange
+        $bucket_name = 'fields';
+        $this->arrangeCreate("INSERT INTO $bucket_name () VALUES ");
+        // Act
+        $this->sut->createBucket($bucket_name, [], []);
+        // Assert
+    }
+
+    public function testCreate_SingleFieldNoValues_Query() {
+        // Arrange
+        $bucket_name = 'fields';
+        $field_id = 'field';
+        $this->arrangeCreate("INSERT INTO $bucket_name ($field_id) VALUES ");
+        // Act
+        $this->sut->createBucket($bucket_name, [$field_id], []);
+        // Assert
+    }
+
+    public function testCreate_MultipleFieldMultipleValues_Query() {
+        // Arrange
+        $bucket_name = 'fields';
+        $field_id = 'field';
+        $field_id2 = 'field2';
+        $value_1_field_id = 'a';
+        $value_1_field_id2 = 'b';
+        $value_2_field_id = 'c';
+        $value_2_field_id2 = 'd';
+        $this->arrangeCreate("INSERT INTO $bucket_name ($field_id,$field_id2) VALUES ('$value_1_field_id','$value_1_field_id2'),('$value_2_field_id','$value_2_field_id2')");
+        // Act
+        $this->sut->createBucket($bucket_name, [$field_id, $field_id2], [[$value_1_field_id, $value_1_field_id2], [$value_2_field_id, $value_2_field_id2]]);
+        // Assert
     }
 }
 ?>
