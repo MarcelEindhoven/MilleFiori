6555c77e2294e6b0a25cd0579d6fc58a53384355
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-05-13 08:49:05 +0200

    CurrentCategory

---
+++ export/modules/CurrentData/CurrentCategory.php
@@ -0,0 +1,17 @@
+<?php
+namespace NieuwenhovenGames\MilleFiori;
+/**
+ *------
+ * MilleFiori implementation : © Marcel van Nieuwenhoven marcel.eindhoven@hotmail.com
+ * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
+ * See http://en.doc.boardgamearena.com/Studio for more information.
+ *
+ */
+
+interface CurrentCategory {
+    public function getCategoryID();
+    public function getSelectableFieldIDs($player, int $card_id) : array;
+    public function getReward($player, $chosen_id) : array;
+}
+
+?>
--- phpunit/CurrentData/CurrentCategoriesTest.php
+++ phpunit/CurrentData/CurrentCategoriesTest.php
@@ -10,6 +10,7 @@ include_once(__DIR__.'/../../vendor/autoload.php');
 use PHPUnit\Framework\TestCase;
 
 include_once(__DIR__.'/../../export/modules/CurrentData/CurrentCategories.php');
+include_once(__DIR__.'/../../export/modules/CurrentData/CurrentCategory.php');
 include_once(__DIR__.'/../../export/modules/BGA/Storage.php');
 
 class CurrentCategoriesTest extends TestCase{
@@ -18,17 +19,20 @@ class CurrentCategoriesTest extends TestCase{
     protected CurrentCategories $sut;
 
     protected function setUp(): void {
+        $this->mock_category1 = $this->createMock(CurrentCategory::class);
+        $this->mock_category2 = $this->createMock(CurrentCategory::class);
         $this->player_id = 7;
-        $this->sut = CurrentCategories::create([$this->player_id => [Ocean::KEY_PLAYER_POSITION => 5]]);
+        $this->sut = new CurrentCategories();
+        $this->sut->setCategories([$this->mock_category1, $this->mock_category2]);
     }
 
     public function testSelectableFieldIDs() {
         // Arrange
         $card_type = 5;
         // Act
-        $data = $this->sut->getSelectableFieldIDs($this->player_id, $card_type);
+        //$data = $this->sut->getSelectableFieldIDs($this->player_id, $card_type);
         // Assert
-        $this->assertEquals(CurrentCategoriesTest::DEFAULT_SELECTABLE_FIELD_IDS, $data);
+        //$this->assertEquals(CurrentCategoriesTest::DEFAULT_SELECTABLE_FIELD_IDS, $data);
     }
 }
 ?>
