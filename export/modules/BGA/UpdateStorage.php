3ced74576c478ad5e52c9583513a45efd308eca6
MarcelEindhoven <marcel.eindhoven@gmail.com>
2023-05-13 07:31:28 +0200

    event

--- export/modules/BGA/UpdateStorage.php
+++ export/modules/BGA/UpdateStorage.php
@@ -11,6 +11,11 @@ namespace NieuwenhovenGames\BGA;
 include_once(__DIR__.'/DatabaseInterface.php');
 
 class UpdateStorage {
+    const EVENT_KEY_BUCKET = 'channel';
+    const EVENT_KEY_NAME_VALUE = 'field_name_value';
+    const EVENT_KEY_UPDATED_VALUE = 'new_value';
+    const EVENT_KEY_NAME_SELECTOR = 'field_name_selector';
+    const EVENT_KEY_SELECTED = 'selected_field';
 
     static public function create($sql_database) : UpdateStorage {
         $object = new UpdateStorage();
@@ -22,8 +27,20 @@ class UpdateStorage {
         return $this;
     }
 
+    public function setEventEmitter($event_emitter) : UpdateStorage {
+        $this->event_emitter = $event_emitter;
+        return $this;
+    }
+
     public function updateValueForField($bucket_name, $field_name_value, $value, $field_name_selector, $value_selector) {
         $this->sql_database->query("UPDATE $bucket_name SET $field_name_value=$value WHERE $field_name_selector=$value_selector");
+        $event = [
+            UpdateStorage::EVENT_KEY_BUCKET => $bucket_name,
+            UpdateStorage::EVENT_KEY_NAME_VALUE => $field_name_value,
+            UpdateStorage::EVENT_KEY_UPDATED_VALUE => $value,
+            UpdateStorage::EVENT_KEY_NAME_SELECTOR => $field_name_selector,
+            UpdateStorage::EVENT_KEY_SELECTED => $value_selector];
+        $this->event_emitter->emit($bucket_name, $event);
     }
 }
 ?>
--- phpunit/BGA/UpdateStorageTest.php
+++ phpunit/BGA/UpdateStorageTest.php
@@ -18,29 +18,36 @@ class UpdateStorageTest extends TestCase{
 
     protected function setUp(): void {
         $this->mock_database = $this->createMock(\NieuwenhovenGames\BGA\DatabaseInterface::class);
+        $this->sut = \NieuwenhovenGames\BGA\UpdateStorage::create($this->mock_database);
 
         $this->mock_emitter = $this->createMock(\NieuwenhovenGames\BGA\EventEmitter::class);
-
-        $this->sut = \NieuwenhovenGames\BGA\UpdateStorage::create($this->mock_database);
+        $this->sut->setEventEmitter($this->mock_emitter);
     }
 
-    public function testUpdate_Value_Query() {
-        // Arrange
-        $bucket_name = 'field';
-        $field_name_value = 'player_id';
-        $value = '3';
-        $field_name_selector = 'field_id';
-        $value_selector = 'field_ocean_3';
-        $this->arrangeQuery("UPDATE $bucket_name SET $field_name_value=$value WHERE $field_name_selector=$value_selector");
-        // Act
-        $this->sut->updateValueForField($bucket_name, $field_name_value, $value, $field_name_selector, $value_selector);
-        // Assert
+    protected function arrangeDefault() {
+        $this->bucket_name = 'field';
+        $this->field_name_value = 'player_id';
+        $this->value = '3';
+        $this->field_name_selector = 'field_id';
+        $this->value_selector = 'field_ocean_3';
+        $this->arrangeQuery("UPDATE $this->bucket_name SET $this->field_name_value=$this->value WHERE $this->field_name_selector=$this->value_selector");
+
+        $this->mock_emitter->expects($this->exactly(1))
+        ->method('emit');
     }
 
     protected function arrangeQuery($expected_query) {
         $this->mock_database->expects($this->exactly(1))
         ->method('query')
         ->with($this->equalTo($expected_query));
+    }
+
+    public function testUpdate_Value_Query() {
+        // Arrange
+        $this->arrangeDefault();
+        // Act
+        $this->sut->updateValueForField($this->bucket_name, $this->field_name_value, $this->value, $this->field_name_selector, $this->value_selector);
+        // Assert
     }
 }
 ?>
